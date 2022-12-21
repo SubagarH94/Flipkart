cdfc17e220e879a3544a952c65ce8f956c0dd684
SubagarH94 <subagarh94@gmail.com>
2022-12-21 16:38:44 +0530

    adding base class lines to haridoss

--- src/test/java/org/baseclass/Baseclass.java
+++ src/test/java/org/baseclass/Baseclass.java
@@ -1,6 +1,12 @@
 package org.baseclass;
 
 public class Baseclass {
+public static void base() {
+	System.out.println("Haridoss 1");
+	System.out.println("Haridoss 2");
+	System.out.println("Haridoss 3");
+	
+}
 
 	
 	
