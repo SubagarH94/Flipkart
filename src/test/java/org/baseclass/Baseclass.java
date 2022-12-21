6de65e89a154c3bf2200af66db177ff09422dcdf
SubagarH94 <subagarh94@gmail.com>
2022-12-21 16:46:00 +0530

    adding sysout to subagar

--- src/test/java/org/baseclass/Baseclass.java
+++ src/test/java/org/baseclass/Baseclass.java
@@ -1,6 +1,11 @@
 package org.baseclass;
 
 public class Baseclass {
+	public static void basee() {
+		System.out.println("Subagar 1");
+		System.out.println("Subagar 2");
+		System.out.println("Subagar 3");
+	}
 
 	
 	
